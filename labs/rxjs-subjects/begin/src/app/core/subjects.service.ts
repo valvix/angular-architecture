d854cf82a1593e8191767bfa0728baa487faa650
valvix <bolboaca_vali@yahoo.com>
2021-10-24 18:11:36 +0300

    exercise 1

--- labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
+++ labs/rxjs-subjects/begin/src/app/core/subjects.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsService {
-  
-  //Define Subject and Observable properties here
 
+  //Define Subject and Observable properties here
+  public obervable$: Observable<string>;
+  private subject$: Subject<string>;
 
   constructor() {
     this.init();
@@ -15,10 +17,15 @@ export class SubjectsService {
 
   init() {
     // Create Subject and Observable Here 
-
+    this.subject$ = new Subject();
+    this.obervable$ = this.subject$.asObservable();
 
     // Create interval here
-
+    setInterval(() => {
+      const date = new Date();
+      const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+      this.subject$.next(time);
+    }, 3000)
   }
 
 }
